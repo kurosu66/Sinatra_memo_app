d97fea79b34e4f36f2412b82a1fa5e6814ce9daa
kurosu66 <kurosu.66@gmail.com>
2022-11-10 23:04:29 +0900

    sql実行にプリペアドステートメントを使用するよう修正

--- app.rb
+++ app.rb
@@ -2,8 +2,6 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
-require 'json'
-require 'securerandom'
 require 'pg'
 
 helpers do
@@ -24,40 +22,46 @@ get '/new' do
 end
 
 post '/memos' do
-  conn.exec("INSERT INTO memos (title,content) VALUES ('#{params[:title]}','#{params[:content]}')")
+  conn.exec_params(
+    %q{INSERT INTO memos (title, content) VALUES ($1, $2)},
+    ["#{params[:title]}", "#{params[:content]}"]
+  )
   redirect '/memos'
 end
 
 get '/memos/:id' do
   @memo_id = params[:id]
-  conn.exec("SELECT * FROM memos where id = #{params[:id]}") do |result|
+  sql = %q{SELECT * FROM memos WHERE id = ($1)}
+  conn.exec_params(sql,[params[:id]]) do |result|
     result.each do |row|
       @memo_title = row["title"]
       @memo_content = row["content"]
-    end
+    end 
   end
-
   erb :detail
 end
 
 get '/memos/:id/edit' do
   @memo_id = params[:id]
-  conn.exec("SELECT * FROM memos where id = #{params[:id]}") do |result|
+  sql = %q{SELECT * FROM memos WHERE id = ($1)}
+  conn.exec_params(sql,[params[:id]]) do |result|
     result.each do |row|
       @memo_title = row["title"]
       @memo_content = row["content"]
-    end
+    end 
   end
 
   erb :edit
 end
 
 patch '/memos/:id' do
-  conn.exec("UPDATE memos SET title = '#{params[:title]}', content = '#{params[:content]}' WHERE id = #{params[:id]}")
+  sql = %q{UPDATE memos SET title = ($1), content = ($2) WHERE id = ($3)}
+  conn.exec_params(sql,[params[:title], params[:content], params[:id]])
   redirect '/memos'
 end
 
 delete '/memos/:id' do
-  conn.exec("delete from memos WHERE id = #{params[:id]}")
+  sql = %q{delete from memos WHERE id = ($1)}
+  conn.exec_params(sql, [params[:id]])
   redirect '/memos'
 end
